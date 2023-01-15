5e25d56dc52cc561f18b5ee3ad05558a07691406
Olivier-Kango <olivierkango@gmail.com>
2023-01-15 07:42:33 -0800

    Move queries to queries.sql

--- sql_training/shema.sql
+++ sql_training/shema.sql
@@ -13,5 +13,3 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2020-11-15', 2, TRUE, 8);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
-
-SELECT * FROM animals WHERE name LIKE '%mon';
