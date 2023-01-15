0579970b9e8f9777b8fe1a957dfddbd4c3aacba7
Olivier-Kango <olivierkango@gmail.com>
2023-01-15 07:19:34 -0800

    Add data in animals

--- sql_training/shema.sql
+++ sql_training/shema.sql
@@ -1,12 +1,15 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals(
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
-  date_of_birth DATE,
+  date_of_birth DATE NOT NULL,
   escape_attempts INT,
   neutered BOOLEAN,
-  wight_kg DEC
+  weight_kg DEC
 );
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, wight_kg) VALUES ('Augmon', '2001-07-13', 0, FALSE, 12.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2020-11-15', 2, TRUE, 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
